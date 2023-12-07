769eccb505302b900e4e4083e708d9effe85bbd8
21120150 <toancva153@gmail.com>
2023-12-08 00:22:45 +0700

    Activity to Fragment

--- app/src/main/java/com/example/sendingdata/MainActivity.java
+++ app/src/main/java/com/example/sendingdata/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.sendingdata;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,36 +12,63 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText name, email, phone;
-    Button signup;
+//    EditText name, email, phone;
+//    Button signup;
+
+    EditText weight, height;
+    Button calculate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        name = findViewById(R.id.editTextName);
-        email = findViewById(R.id.editTextEmail);
-        phone = findViewById(R.id.editTextPhone);
+//        name = findViewById(R.id.editTextName);
+//        email = findViewById(R.id.editTextEmail);
+//        phone = findViewById(R.id.editTextPhone);
+//
+//        signup = findViewById(R.id.buttonSignup);
+//
+//        signup.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String userName = name.getText().toString();
+//                String userEmail = email.getText().toString();
+//                int userPhone = Integer.parseInt(phone.getText().toString());
+//
+//                Intent i = new Intent(MainActivity.this, SecondActivity.class);
+//                i.putExtra("name", userName);
+//                i.putExtra("email", userEmail);
+//                i.putExtra("phone", userPhone);
+//                startActivity(i);
+//
+//            }
+//        });
 
-        signup = findViewById(R.id.buttonSignup);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        MyFirstFragment myFirstFragment = new MyFirstFragment();
+        weight = findViewById(R.id.editTextWeight);
+        height = findViewById(R.id.editTextHeight);
+        calculate = findViewById(R.id.buttonCalculate);
 
-        signup.setOnClickListener(new View.OnClickListener() {
+        calculate.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                String userName = name.getText().toString();
-                String userEmail = email.getText().toString();
-                int userPhone = Integer.parseInt(phone.getText().toString());
+            public void onClick(View v) {
+                Bundle bundle = new Bundle();
+                int userWeight = Integer.valueOf(weight.getText().toString());
+                int userHeight = Integer.valueOf(height.getText().toString());
 
-                Intent i = new Intent(MainActivity.this, SecondActivity.class);
-                i.putExtra("name", userName);
-                i.putExtra("email", userEmail);
-                i.putExtra("phone", userPhone);
-                startActivity(i);
+                bundle.putInt("weight", userWeight);
+                bundle.putInt("height", userHeight);
+
+                myFirstFragment.setArguments(bundle);
+                fragmentTransaction.add(R.id.frame, myFirstFragment);
+                fragmentTransaction.commit();
+
 
             }
         });
-
 
     }
 }
---
+++ app/src/main/java/com/example/sendingdata/MyFirstFragment.java
@@ -0,0 +1,37 @@
+package com.example.sendingdata;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class MyFirstFragment extends Fragment {
+
+    TextView result;
+
+    public MyFirstFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View view = inflater.inflate(R.layout.fragment_my_first, container, false);
+        result = view.findViewById(R.id.textViewResult);
+        Bundle bundle = getArguments();
+        int userWeight = bundle.getInt("weight");
+        int userHeight = bundle.getInt("height");
+
+        double userBmi = (userWeight * 1000) / (userHeight * userHeight);
+
+        result.setText("Your BMI is: " + userBmi);
+
+        return view;
+    }
+}
